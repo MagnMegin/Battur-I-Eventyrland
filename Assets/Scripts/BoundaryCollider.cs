3648d24bd3994e95269ac932e7c13cfc216236df
MagnMegin <magne.myran@skomakerhuset.no>
2023-05-04 14:26:00 +0200

    Ferdigstilt boundarycollider.

--- Assets/Scripts/BoundaryCollider.cs
+++ Assets/Scripts/BoundaryCollider.cs
@@ -11,7 +11,6 @@ public class BoundaryCollider : MonoBehaviour
     public void Reset()
     {
         EdgeCollider = gameObject.AddComponent<EdgeCollider2D>();
-        EdgeCollider.hideFlags= HideFlags.HideInInspector;
         EdgeCollider.points = new Vector2[]
         {
             new Vector2(-1, -1),
@@ -24,16 +23,9 @@ public class BoundaryCollider : MonoBehaviour
 
     private void Update()
     {
-        if (EdgeCollider == null) EdgeCollider = GetComponent<EdgeCollider2D>();
-
         Vector2[] newPoints = EdgeCollider.points;
         newPoints[0] = newPoints.Last();
         EdgeCollider.points = newPoints;
-    }
-
-    private void OnDestroy()
-    {
-        DestroyImmediate(EdgeCollider);
     }
 #endif
 }
