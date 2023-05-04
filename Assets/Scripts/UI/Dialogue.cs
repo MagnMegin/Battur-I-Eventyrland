cd6dcb58f6bce7921a00fa1b31f058593e537d57
AndreanoEssobar <73395818+AndreanoEssobar@users.noreply.github.com>
2023-05-04 20:32:10 +0200

    Added dialogue scene & script (also TextMP)

---
+++ Assets/Scripts/UI/Dialogue.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialogue : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+    public string[] lines;
+    public float textSpeed;
+
+    private int index;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent.text = string.Empty; //Makes sure the textcomponent is empty on scene start
+        StartDialogue();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //This is to skip the sentence being written out letter for letter, or to skip to the next sentence.
+        if (Input.GetMouseButtonDown(0))
+        {
+            //If the current line is finished typing, run this
+            if(textComponent.text == lines[index])
+            {
+                NextLine();
+            }
+
+            //If the current line is still being written out, complete it.
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+            }
+        }
+    }
+
+    void StartDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine() //The aim of this coroutine is to make the text appear one letter after the other
+    {
+        //This foreach loop breaks all text down to their own letters
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void NextLine() //Completes the sentence being written out or sets the textbox to inactive if there is no more text.
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
