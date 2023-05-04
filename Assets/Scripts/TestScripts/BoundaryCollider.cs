98f9c2a6e09f5b9c4e5c3fca204c7462e0f18119
MagnMegin <magne.myran@skomakerhuset.no>
2023-05-04 14:11:11 +0200

    Created BoundaryCollider class

--- Assets/Scripts/TestScripts/BoundaryCollider.cs
+++ Assets/Scripts/TestScripts/BoundaryCollider.cs
@@ -1,18 +1,39 @@
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
+[ExecuteAlways]
 public class BoundaryCollider : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public EdgeCollider2D EdgeCollider;
+
+#if UNITY_EDITOR
+    public void Reset()
     {
-        
+        EdgeCollider = gameObject.AddComponent<EdgeCollider2D>();
+        EdgeCollider.hideFlags= HideFlags.HideInInspector;
+        EdgeCollider.points = new Vector2[]
+        {
+            new Vector2(-1, -1),
+            new Vector2(-1, 1),
+            new Vector2(1, 1),
+            new Vector2(1, -1),
+            new Vector2(-1, -1),
+        };
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (EdgeCollider == null) EdgeCollider = GetComponent<EdgeCollider2D>();
+
+        Vector2[] newPoints = EdgeCollider.points;
+        newPoints[0] = newPoints.Last();
+        EdgeCollider.points = newPoints;
     }
+
+    private void OnDestroy()
+    {
+        DestroyImmediate(EdgeCollider);
+    }
+#endif
 }
