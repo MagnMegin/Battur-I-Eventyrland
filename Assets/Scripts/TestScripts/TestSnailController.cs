5fc13cff6458332c8b0fc94127dd671e93d2b9b7
MagnMegin <magne.myran@skomakerhuset.no>
2023-05-04 11:27:36 +0200

    Setting up character controller

--- Assets/Scripts/TestScripts/TestSnailController.cs
+++ Assets/Scripts/TestScripts/TestSnailController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OrthoSnailController : MonoBehaviour
+public class TestSnailController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
