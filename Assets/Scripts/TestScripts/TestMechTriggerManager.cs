237ac45fef0e0f2923d56735ecd33d017c0a5a80
Aageus <113115734+Aageus@users.noreply.github.com>
2023-05-12 09:18:28 +0200

    broMech klart
    2 samlede planker gir tilgang til bro over elv

--- Assets/Scripts/TestScripts/TestMechTriggerManager.cs
+++ Assets/Scripts/TestScripts/TestMechTriggerManager.cs
@@ -8,6 +8,9 @@ public class TestMechTriggerManager : MonoBehaviour
     public GameObject CollectedPlank1;
     public GameObject CollectedPlank2;
     public GameObject CollectedPlank3;
+    public GameObject skipUtenMech;
+    public GameObject skipKlarTilBro;
+    private bool _mechShipHasBeenActivated;
     
     void Start()
     {
@@ -17,9 +20,13 @@ public class TestMechTriggerManager : MonoBehaviour
     
     void Update()
     {
-        if (CollectedPlank1.activeInHierarchy && CollectedPlank2.activeInHierarchy)
+        if (CollectedPlank1.activeInHierarchy && CollectedPlank2.activeInHierarchy && !_mechShipHasBeenActivated)
         {
-            Debug.Log("2 planks active");
+            skipKlarTilBro.SetActive(true);
+            skipUtenMech.SetActive(false);
+            _mechShipHasBeenActivated = true;
         }
     }
+
+
 }
