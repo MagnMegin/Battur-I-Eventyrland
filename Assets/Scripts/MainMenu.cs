b0194da179cc56969f639773321b9a1c95ea57d5
MagnMegin <magne.myran@skomakerhuset.no>
2023-05-30 12:36:51 +0200

    Nye sprites

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,17 +1,37 @@
+using FMOD;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public PlayableDirector AnimationDirector;
+    private bool _isStarting;
+
     public void StartGame()
     {
-        SceneManager.LoadSceneAsync(2);
+        _isStarting = true;
+        StartCoroutine(StartGameSequence());
+    }
+
+    private IEnumerator StartGameSequence()
+    {
+        AsyncOperation sceneLoad = SceneManager.LoadSceneAsync(2);
+        sceneLoad.allowSceneActivation = false;
+        AnimationDirector.Play();
+        while (AnimationDirector.state == PlayState.Playing)
+        {
+            yield return null;
+        }
+        sceneLoad.allowSceneActivation = true;
     }
 
     public void QuitGame()
     {
+        if (_isStarting) return;
+
         Application.Quit();
     }
 }
