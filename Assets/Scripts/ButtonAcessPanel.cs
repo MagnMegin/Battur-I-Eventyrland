f6e957a79643ce8f14f72e711f5e7c1c7b3eaf2e
MagnMegin <magne.myran@skomakerhuset.no>
2023-05-31 17:44:25 +0200

    Fininshed

--- Assets/Scripts/ButtonAcessPanel.cs
+++ Assets/Scripts/ButtonAcessPanel.cs
@@ -1,26 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class ButtonAcessPanel : MonoBehaviour
 {
-    public static GameObject Obj1;
-    public static GameObject Obj2;
-    public static GameObject Obj3;
+    public static GameObject Player1;
+    public static GameObject Player2;
 
-    public void CallMethodOnObject1(string MethodName)
+    public void CallMethodOnPlayer1(string MethodName)
     {
-        Obj1.SendMessage(MethodName, SendMessageOptions.DontRequireReceiver);
+        Player1.SendMessage(MethodName, SendMessageOptions.DontRequireReceiver);
     }
 
-    public void CallMethodOnObject2(string MethodName)
+    public void CallMethodOnplayer2(string MethodName)
     {
-        Obj2.SendMessage(MethodName, SendMessageOptions.DontRequireReceiver);
-    }
-
-    public void CallMethodOnObject3(string MethodName)
-    {
-        Obj3.SendMessage(MethodName, SendMessageOptions.DontRequireReceiver);
+        Player2.SendMessage(MethodName, SendMessageOptions.DontRequireReceiver);
     }
 }
