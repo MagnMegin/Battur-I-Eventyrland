2fdbc84c8a290f7c031c7ebccd28ab6ebdb59930
MagnMegin <magne.myran@skomakerhuset.no>
2023-05-31 14:41:18 +0200

    La til Elv Ambiance

---
+++ Assets/Scripts/Audio/ElvEmitter.cs
@@ -0,0 +1,11 @@
+using FMODUnity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElvEmitter : MonoBehaviour
+{
+    public EventReference ElvEvent;
+
+
+}
