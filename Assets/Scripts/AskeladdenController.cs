45c4c3f554d34350781ac0faf6b38580c40b42d5
MagnMegin <magne.myran@skomakerhuset.no>
2023-06-01 13:12:46 +0200

    Finished animation

--- Assets/Scripts/AskeladdenController.cs
+++ Assets/Scripts/AskeladdenController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class AskeladdenController : MonoBehaviour
 {
     public float MoveSpeed;
+    public Animator Anim;
+    public SpriteRenderer Renderer;
     
     private Vector3 _moveDirection;
     private Rigidbody2D _rb;
@@ -18,11 +20,34 @@ public class AskeladdenController : MonoBehaviour
     private void Update()
     {
         _moveDirection = InputManager.GetMovement();
+        UpdateAnimation();
     }
 
     private void FixedUpdate()
     {
         _rb.velocity = _moveDirection * MoveSpeed;
+    }
+
+    private void UpdateAnimation()
+    {
+        if (_moveDirection == Vector3.zero)
+        {
+            Anim.SetBool("IsMoving", false);
+            return;
+        }
+
+        Anim.SetBool("IsMoving", true);
+        Anim.SetFloat("X", _moveDirection.x);
+        Anim.SetFloat("Y", _moveDirection.y);
+
+        if(_moveDirection.x < -0.1)
+        {
+            Renderer.flipX = true;
+        }
+        else if (_moveDirection.x > 0.1)
+        {
+            Renderer.flipX = false; 
+        }
     }
     #endregion
 }
